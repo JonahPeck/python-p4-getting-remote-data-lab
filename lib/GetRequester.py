d96abd631d09a2268a2f21f457de0adef0e6f111
JonahPeck <PeckJonahC@gmail.com>
2023-04-04 08:52:06 -0600

    Making sense

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,33 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+
+        return json.loads(self.get_response_body())
+
+
+get_requester = GetRequester(
+    'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
+print(get_requester)
+
+
+# url = "https://jsonplaceholder.typicode.com/todos/1"
+# requester = GetRequester(url)
+# response_body = requester.get_response_body()
+# todo = requester.load_json()
+
+# for response in set(response_body):
+
+#     print(response)
